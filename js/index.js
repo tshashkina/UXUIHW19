ee4dd620bfdf38e0b4c3e470134aeea7dcdac227
tshashkina <147959691+tshashkina@users.noreply.github.com>
2024-03-18 15:56:51 -0600

    hw19 (half)

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/* Test to make sure your JavaScript file is loaded corectly */
+
+/*console.log("Your index.js file is loaded correctly");*/
+
+/*$("button").hover(function() {
+    console.log("You hovered on button");
+});*/
+
+$("button").hover(function() {
+    $(this).css('background-color', 'grey');
+}, function() {
+    $(this).css('background-color', 'transparent');
+});
+
+
+$("button").click(function() {
+    $(this).css('background-color', 'white');
+    $(this).css('color', 'black');
+
+});
+
+
